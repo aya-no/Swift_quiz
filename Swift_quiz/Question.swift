5b564af698b5584850dd657a965f727cfca1ad63
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-06-13 21:36:41 +0900

    初期画面

---
+++ Swift_quiz/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Swift_quiz
+//
+//  Created by user on 2023/06/13.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+}
--- Swift_quiz/ViewController.swift
+++ Swift_quiz/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    let quiz = [
+        Question(text: "第1問", answer: "true"),
+        Question(text: "第2問", answer: "false"),
+        Question(text: "第3問", answer: "true")
+    ]
+    
+    var questionNum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        updataUI()
+
     }
 
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualQuestion = quiz[questionNum]
+        let actualAnswer = actualQuestion.answer
+        
+        if userAnswer == actualAnswer {
+            sender.backgroundColor = UIColor.green
+            print("正解")
+        } else {
+            sender.backgroundColor = UIColor.red
+            print("ざんねん")
+        }
+        
+        if questionNum < quiz.count - 1 {
+            questionNum += 1
+        } else {
+            questionNum = 0
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updataUI), userInfo: nil, repeats: false)
 
+    }
+    
+    @objc func updataUI() {
+        questionLabel.text = quiz[questionNum].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNum + 1) / Float(quiz.count)
+    }
+    
 }
 
