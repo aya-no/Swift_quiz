45927b8af192663f0554c679325c61dd91304f25
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-06-13 10:10:51 +0900

    Initial Commit

---
+++ Swift_quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_quiz
+//
+//  Created by user on 2023/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
